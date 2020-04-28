5c6bb43e74f5d938d270ce528547211296573243
giddens9527 <jl4386@outlook.com>
2020-04-28 23:05:14 +0800

    Basic repetitive repl

---
+++ prompt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+static char input[2048];
+
+int main(int argc, char **argv) {
+  puts("Lispy Version 0.0.0.0.1");
+  puts("Press Cmd + c to exit");
+
+  while (1) {
+    fputs("listpy> ", stdout);
+    fgets(input, 2048, stdin);
+    printf("No you're a %s", input);
+  }
+  return 0;
+}
